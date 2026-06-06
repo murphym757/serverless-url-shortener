fbeaf6a3789890a05d6465126164fa452b45c412
Maurice Murphy <murphym757@yahoo.com>
2026-06-06 18:16:37 -0400

    chore: extract terraform to dedicated infrastructure repo

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,21 @@ terraform {
     required_version = ">= 1.0"
 }
 
+resource "aws_dynamodb_table" "url_shortener" {
+    name         = "UrlShortener"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key     = "shortCode"
+
+    attribute {
+        name = "shortCode"
+        type = "S"
+    }
+
+    tags = {
+        Project = "serverless-url-shortener"
+    }
+}
+
 provider "aws" {
     region = "us-east-1"
 }
